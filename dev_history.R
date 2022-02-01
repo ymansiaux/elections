4d178040d7582f3038496019dbe584e39847e6d5
Yohann Mansiaux <y.mansiaux@bordeaux-metropole.fr>
2022-02-01 14:22:17 +0100

    commit initial

---
+++ dev_history.R
@@ -0,0 +1,41 @@
+usethis::use_build_ignore("dev_history.R")
+
+usethis::use_build_ignore("deploy_to_RSPM.R")
+
+usethis::use_r("_disable_autoload")
+usethis::use_build_ignore("R/_disable_autoload.R")
+
+renv::init()
+renv::status()
+renv::snapshot()
+# usethis::use_build_ignore("vignettes/occupationparkingsapp")
+# usethis::use_build_ignore("vignettes/questions")
+# usethis::use_build_ignore("vignettes/designApp")
+
+
+
+usethis::use_data_raw("parkings")
+
+usethis::use_vignette("occupationparkingsapp")
+usethis::use_vignette("questions")
+usethis::use_vignette("designApp")
+
+
+
+
+attachment::att_amend_desc()
+vignette <- FALSE
+devtools::check(document = TRUE, vignettes = vignette)
+devtools::build(vignettes = vignette)
+devtools::install(build_vignettes = vignette)
+
+usethis::use_test("occupation_compute_xtradata_request_parameters")
+
+
+pkgload::load_all()
+
+golem::sanity_check()
+
+golem::run_dev()
+
+prefixer::show_nonascii_file()
