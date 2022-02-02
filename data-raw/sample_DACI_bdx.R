366f0b11e748dd1db2e1b75f9e1f954878ca639e
Yohann Mansiaux <y.mansiaux@bordeaux-metropole.fr>
2022-02-02 10:50:00 +0100

    issue 3 solved

---
+++ data-raw/sample_DACI_bdx.R
@@ -0,0 +1,4 @@
+## code to prepare `sample_DACI_bdx` dataset goes here
+sample_DACI_bdx <- data.table::fread("/data/data_elections/DACI/DACI_PIVOT_ELECTION_UTF8.csv", encoding = 'UTF-8')
+
+usethis::use_data(sample_DACI_bdx, overwrite = TRUE)
