e14d38ab8cb2cda8965d0cb757d98f409458e568
Yohann Mansiaux <y.mansiaux@bordeaux-metropole.fr>
2022-02-03 11:29:36 +0100

    Issue 5 solved

--- R/app_server.R
+++ R/app_server.R
@@ -5,20 +5,47 @@
 #' @import shiny
 #' @import data.table
 #' @import sf
-#' @importFrom shinyYM closeWaiter
+#' @importFrom shinyYM closeWaiter add_notie_alert
+#' @importFrom xtradata xtradata_requete_features
 #' @noRd
 app_server <- function( input, output, session ) {
   # Your application server logic 
   options(datatable.print.class = TRUE)
   options(bitmapType = "cairo")
   
-  observe(closeWaiter(TRUE, 3))
+  observe(closeWaiter(golem::app_prod(), 3))
+  
+  
+  # Chargement des données au démarrage
+  data_elections <- reactiveValues(data = NULL)
+  
+  observeEvent(session, {
+    
+    dat <- try(xtradata_requete_features(key = Sys.getenv("XTRADATA_KEY"), typename = "ST_PARK_P", showURL = TRUE))
+
+    if(inherits(dat, "try-error")) {
+      add_notie_alert(type = "error", text = "Echec de récupération des données",
+                      stay = TRUE, time = 5, position = "bottom", session)
+
+    } else {
+      add_notie_alert(type = "success", text = "Connexion à la base OK",
+                      stay = FALSE, time = 5, position = "bottom", session)
+      
+      data_elections$data <- dat
+    }
+    
+    
+  }, ignoreNULL = FALSE, once = TRUE)
+  
+  # output$print_data <- renderPrint(
+  #   head(data_elections$data)
+  # )
   
   
   ### PARTIE BDXMETROIDENTITY ###
   rv <- reactiveValues()
   rv$theme <- "light"
-
+  
   output$my_logo <- renderUI({
     if (rv$theme == "light") {
       tags$img(src = "www/datalab-logo-lightmode.png", width = "150px")
